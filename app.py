4c7b21969dc0436004ed5e81ad7b7271d874a427
Maagnitude <yiwrgos_v.i.p@hotmail.com>
2023-04-04 16:02:52 +0300

    created structure

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
-from flask import render_template, redirect, request, url_for, flash, abort
-from flask_login import login_user, login_required, logout_user
+from companyblog import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
